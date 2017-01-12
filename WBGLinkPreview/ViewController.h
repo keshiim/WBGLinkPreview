3e538cc515037f27fde1002104cb81414bedd5ad
zhengmingjun <keshiim@163.com>
2017-01-12 16:57:14 +0800

    Initial Commit

---
+++ WBGLinkPreview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBGLinkPreview
+//
+//  Created by Jason on 2017/1/12.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WBGLinkPreview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WBGLinkPreview
+//
+//  Created by Jason on 2017/1/12.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
